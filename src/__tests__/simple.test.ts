c49489004b5151eb865b899054fedc8291cd93d7
Jonas Kello <jonas.kello@gmail.com>
2021-04-04 15:13:08 +0200

    Add more tests

--- src/__tests__/simple.test.ts
+++ src/__tests__/simple.test.ts
@@ -1,8 +1,30 @@
-import { ctorsUnion } from "../ctors-union";
+import { CtorsUnion, ctorsUnion, ctorsUnionWithTypeKey } from "../ctors-union";
 
 describe("Simple", () => {
   it("should not crash", () => {
     const action = ctorsUnion({});
     expect(action).toBeTruthy();
+  });
+  it("should create action", () => {
+    const Action = ctorsUnion({
+      Foo: (foo: string) => ({ foo }),
+      Bar: (bar: number) => ({ bar }),
+      Curry: (one: string) => (two: number) => ({ one, two }),
+    });
+    type Action = CtorsUnion<typeof Action>;
+    const fooAction = Action.Foo("myfoo");
+    expect(fooAction).toEqual({ type: "Foo", foo: "myfoo" });
+  });
+  it("should create action with other type key", () => {
+    const Action = ctorsUnionWithTypeKey("tag", {
+      Foo: (foo: string) => ({ foo }),
+      Bar: (bar: number) => ({ bar }),
+      Curry: (one: string) => (two: number) => ({ one, two }),
+    });
+    type Action = CtorsUnion<typeof Action>;
+    const fooAction = Action.Foo("myfoo");
+    expect(fooAction).toEqual({ tag: "Foo", foo: "myfoo" });
+    // Check that the types allow us to access .tag
+    expect(fooAction.tag).toEqual("Foo");
   });
 });
